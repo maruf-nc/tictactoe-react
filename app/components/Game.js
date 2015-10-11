66dfa6f14dab551b04ecd12cd50a644e61b5d7fd
Maruf Siddiqui <maruf@newscred.com>
2015-10-11 11:36:40 +0600

    Add Game component

---
+++ app/components/Game.js
@@ -0,0 +1,99 @@
+'use strict';
+
+import React from 'react';
+import Board from './Board';
+import Player from './Player';
+import GameHeader from './GameHeader';
+import LeaderBoard from './LeaderBoard';
+import {SYMBOL_O, SYMBOL_X} from '../constants';
+
+export default class Game extends React.Component {
+
+  constructor() {
+    super();
+    this.state = this.getState();
+    this.winningNumbers = [7, 56, 448, 73, 146, 292, 273, 84];
+
+    this.tileClick = this.tileClick.bind(this);
+    this.start = this.start.bind(this);
+    this.getCurrentPlayer = this.getCurrentPlayer.bind(this);
+    this.getCurrentSymbol = this.getCurrentSymbol.bind(this);
+  }
+
+  getState() {
+    return {
+      tiles: this.generateTiles(),
+      isGameBeingPlayed: false,
+      round: 0,
+      moves: 0,
+      isX: false,
+      showPlayerFormModal: false,
+      winner: null
+    }
+  }
+
+  generateTiles() {
+    return Array.from((new Array(9)).keys()).map(x => {return {symbol: null, value: Math.pow(2, x)}});
+  }
+
+  start(player1, player2) {
+    this.setState(Object.assign(this.getState(), {isGameBeingPlayed: true}));
+    this.playerWithSymbolO = new Player(SYMBOL_O, player1);
+    this.playerWithSymbolX = new Player(SYMBOL_X, player2);
+  }
+
+  getCurrentSymbol() {
+    return this.state.isX ? SYMBOL_X : SYMBOL_O;
+  }
+
+  getCurrentPlayer() {
+    return this.state.isX ? this.playerWithSymbolX : this.playerWithSymbolO;
+  }
+
+  tileClick(tile) {
+    this.getCurrentPlayer().updateScore(tile.value);
+    this.setState({
+      moves: this.state.moves + 1,
+      tiles: this.state.tiles
+    }, function () {
+      this.checkWinConditions();
+      this.setState({isX: !this.state.isX})
+    });
+  }
+
+  checkWinConditions() {
+    if (this.isCurrentPlayerWinner()) {
+      this.setState({winner: this.getCurrentPlayer().name, isGameBeingPlayed: false});
+    } else if (this.state.moves > 8) {
+      this.setState({winner: 'Nobody', isGameBeingPlayed: false});
+    }
+  }
+
+  isCurrentPlayerWinner() {
+    return this.winningNumbers.some(x => (x & this.getCurrentPlayer().getScore()) === x);
+  }
+
+  render() {
+    return (
+      <div className='row'>
+        <div className="col-md-8">
+          <div className="row">
+            <div className="col-md-12">
+              <GameHeader isGameBeingPlayed={this.state.isGameBeingPlayed}
+                          start={this.start} getCurrentPlayer={this.getCurrentPlayer} />
+            </div>
+            <div className="col-md-12">
+              <Board tiles={this.state.tiles}
+                     isGameBeingPlayed={this.state.isGameBeingPlayed}
+                     getCurrentSymbol={this.getCurrentSymbol}
+                     tileClick={this.tileClick} />
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <LeaderBoard winner={this.state.winner} />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ app/components/__tests__/Game-test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+jest.dontMock('../Board');
+jest.dontMock('../Square');
+jest.dontMock('../Player');
+jest.dontMock('../Game');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactTestUtils = require('react-addons-test-utils');
+
+var Game = require('../Game');
+
+describe('Game', function () {
+
+  var game, gameNode;
+
+  beforeEach(_render);
+
+  it('should render properly', function () {
+    expect(gameNode.querySelectorAll('.square').length).toBe(9);
+  });
+
+  describe('.generateTiles', function () {
+    it('should renerate tiles properly', function () {
+      expect(game.generateTiles().length).toBe(9)
+    });
+  });
+
+  describe('.start', function () {
+    it('should start the game', function () {
+      game.start('', '');
+      expect(game.state.isGameBeingPlayed).toBeTruthy();
+    });
+  });
+
+  describe('.getCurrentSymbol', function () {
+    it('should get current symbol', function () {
+      expect(game.getCurrentSymbol()).toBe('O');
+    });
+  });
+
+  describe('.getCurrentPlayer', function () {
+    it('should get current player', function () {
+      game.start('james', 'tim');
+      expect(game.getCurrentPlayer().name).toBe('james');
+    });
+  });
+
+  describe('.tileClick', function () {
+    it('should update various states', function () {
+      game.start('james', 'tim');
+      game.tileClick({value: 4});
+      expect(game.playerWithSymbolO.getScore()).toBe(4);
+      expect(game.state.moves).toBe(1);
+    });
+  });
+
+  describe('.checkWinConditions', function () {
+    it('should check if the game has a winner or it is a draw', function () {
+      game.start('james', 'tim');
+      game.getCurrentPlayer().updateScore(7);
+      game.checkWinConditions();
+      expect(game.state.isGameBeingPlayed).toBeFalsy();
+      expect(game.state.winner).toBe('james');
+    });
+  });
+
+  describe('.isCurrentPlayerWinner', function () {
+    it('should return boolean if the current player is winner or not', function () {
+      game.start('james', 'tim');
+      game.getCurrentPlayer().updateScore(7);
+      expect(game.isCurrentPlayerWinner()).toBeTruthy();
+    });
+  });
+
+  function _render() {
+    game = ReactTestUtils.renderIntoDocument(
+      <Game />
+    );
+    gameNode = ReactDOM.findDOMNode(game);
+  }
+});
